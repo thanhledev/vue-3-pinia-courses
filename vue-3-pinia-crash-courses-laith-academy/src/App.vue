2b3eedf17616e232f6d6f96baa587bcb07b56c94
ThanhLe <thanhle@pop-os.localdomain>
2023-03-13 18:49:05 +0100

    add getters, actions functions of todoStore

--- vue-3-pinia-crash-courses-laith-academy/src/App.vue
+++ vue-3-pinia-crash-courses-laith-academy/src/App.vue
@@ -1,17 +1,33 @@
 <script setup>
+import {ref} from "vue";
 import {useTodosStore} from "./store/todos";
+import {storeToRefs} from "pinia";
 
-const {todos} = useTodosStore();
+const newTodo = ref("");
+
+const todoStore = useTodosStore();
+const {filteredTodos} = storeToRefs(todoStore);
+const {addTodo, toggleTodo, updateFilterTerm} = todoStore;
+
+const addNewTodo = () => {
+  addTodo(newTodo.value);
+  newTodo.value = "";
+}
 
 </script>
 
 <template>
   <main>
-    <input type="text" name="todo" id="todo" placeholder="Todo name...">
-    <button>Add</button>
-    <div v-for="todo in todos">
-      <h4>{{todo.name}}</h4>
-      <button>Toggle</button>
+    <div>
+      <button @click="() => updateFilterTerm('all')">All</button>
+      <button @click="() => updateFilterTerm('finished')">Finished</button>
+      <button @click="() => updateFilterTerm('unfinished')">Unfinished</button>
+    </div>
+    <input type="text" name="todo" id="todo" placeholder="Todo name..." v-model="newTodo">
+    <button @click="addNewTodo">Add</button>
+    <div v-for="todo in filteredTodos" style="display: flex">
+      <h4 :style="todo.isFinished && {textDecoration: 'line-through'}">{{todo.name}}</h4>
+      <button @click="toggleTodo(todo.id)">Toggle</button>
     </div>
   </main>
 </template>
--- vue-3-pinia-crash-courses-laith-academy/src/store/todos.js
+++ vue-3-pinia-crash-courses-laith-academy/src/store/todos.js
@@ -8,6 +8,45 @@ export const useTodosStore = defineStore("todos", {
                 name: "Clean room",
                 isFinished: false,
             },
+            {
+                id: 2,
+                name: "Clean kitchen",
+                isFinished: true,
+            },
+            {
+                id: 3,
+                name: "Clean toilet",
+                isFinished: false,
+            },
         ],
-    })
+        filterTerm: "all",
+    }),
+    getters: {
+        filteredTodos() {
+            if (this.filterTerm === "finished") {
+                return this.todos.filter((todo) => todo.isFinished)
+            } else if (this.filterTerm === "unfinished") {
+                return this.todos.filter((todo) => !todo.isFinished)
+            } else {
+                return this.todos;
+            }
+        }
+    },
+    actions: {
+        addTodo(text) {
+            if (!text) return;
+            this.todos.push({
+                id: Math.floor(Math.random()*100000),
+                name: text,
+                isFinished: false,
+            });
+        },
+        toggleTodo(id) {
+            const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+            this.todos[todoIndex].isFinished = !this.todos[todoIndex].isFinished;
+        },
+        updateFilterTerm(value) {
+            this.filterTerm = value.toLowerCase();
+        }
+    }
 });
