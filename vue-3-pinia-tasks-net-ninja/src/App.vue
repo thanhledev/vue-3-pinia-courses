9cfd15f6503005b5d23ca34e3d871ab02b8ff203
ThanhLe <thanhle@pop-os.localdomain>
2023-03-16 12:06:57 +0100

    Vue-3-pinia-tasks-net-ninja initial commit

---
+++ vue-3-pinia-tasks-net-ninja/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {useTaskStore} from "./stores/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
+
+const taskStore = useTaskStore();
+</script>
+
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia Logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <!-- tasks list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-3-pinia-tasks-net-ninja/src/components/TaskDetails.vue
@@ -0,0 +1,17 @@
+<script setup>
+const {task} = defineProps(["task"]);
+</script>
+
+<template>
+  <div class="task">
+    <h3>{{task.title}}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
